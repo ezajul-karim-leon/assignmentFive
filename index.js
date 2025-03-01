aa11560d8f0292fefbd69a90082172c45ad48310
ezajul-karim-leon <ezazulkarim02@gmail.com>
2025-03-02 02:08:21 +0600

    dashboard done

--- index.js
+++ index.js
@@ -4,6 +4,25 @@ function disableBtn(id){
    return disableButton;
 }
 
+// increment function
+function valueIncrement(){
+    let increment = document.getElementById('increment');
+    const convertedIncrement = parseInt(increment.innerText);
+    const sum = convertedIncrement + 1;
+    increment.innerText =sum;
+
+    return increment;
+}
+
+// decremetn function 
+function valueDecrement(){
+    let decrement=document.getElementById('decrement');
+    const convertedDecrement = parseInt(decrement.innerText);
+    const reduce = convertedDecrement -1;
+    decrement.innerText = reduce;
+    return decrement;
+}
+
 
 //random color generate
 document.getElementById('themeBtn').addEventListener('click', ()=>{
@@ -11,33 +30,133 @@ document.getElementById('themeBtn').addEventListener('click', ()=>{
         document.getElementById('body').style.backgroundColor = randomColor;
 })
 
-let reduce= document.getElementById('reduce').innerText;
-    let convertedReduce = parseInt(reduce);
+
+// Function to get the formatted date
+function getFormattedDate() {
+    const currentDate = new Date();
+    const options = { weekday: 'short', year: 'numeric', month: 'short', day: '2-digit' };
+    return currentDate.toLocaleDateString('en-US', options);
+}
+const formattedDate = getFormattedDate();
+document.getElementById('date').innerText = formattedDate;
+
+
+// discover div click action 
+document.getElementById('discover').addEventListener('click', ()=>{
+   const some= window.location.href ='qusAndAns.html';
+   console.log(some);
+})
+
+
+//complete button click action
 document.getElementById('btn1').addEventListener('click', ()=>{
     alert('Board updated Successfully.')
     disableBtn('btn1');
 
-    
+    valueIncrement();
+    valueDecrement();
+  
     const currentDate = new Date();
     const currentTime = currentDate.toLocaleTimeString();
-    const titleText = document.getElementById('title').innerText;
-    const activity = document.getElementById('activityLog');
-    // const div = document.createElement('div');
-    const div = document.getElementById('div');
-
+    const titleText = document.getElementById('title1').innerText;
+    const activityList = document.getElementById('activityList');
+    const div = document.createElement('div');
     div.innerHTML=`
     <p class='bg-gray-200 p-2 rounded-xl mt-2 text-start text-sm'>You have completed the task ${titleText} at ${currentTime}</p>
     `
-    // activity.appendChild(div);
+    activityList.appendChild(div);
+})
+document.getElementById('btn2').addEventListener('click', ()=>{
+    alert('Board updated Successfully.')
+    disableBtn('btn2');
 
-    let decrement =convertedReduce-1;
-    reduce=decrement;
+    valueIncrement();
+    valueDecrement();
+  
+    const currentDate = new Date();
+    const currentTime = currentDate.toLocaleTimeString();
+    const titleText = document.getElementById('title2').innerText;
+    const activityList = document.getElementById('activityList');
+    const div = document.createElement('div');
+    div.innerHTML=`
+    <p class='bg-gray-200 p-2 rounded-xl mt-2 text-start text-sm'>You have completed the task ${titleText} at ${currentTime}</p>
+    `
+    activityList.appendChild(div);
+})
+document.getElementById('btn3').addEventListener('click', ()=>{
+    alert('Board updated Successfully.')
+    disableBtn('btn3');
+
+    valueIncrement();
+    valueDecrement();
+  
+    const currentDate = new Date();
+    const currentTime = currentDate.toLocaleTimeString();
+    const titleText = document.getElementById('title3').innerText;
+    const activityList = document.getElementById('activityList');
+    const div = document.createElement('div');
+    div.innerHTML=`
+    <p class='bg-gray-200 p-2 rounded-xl mt-2 text-start text-sm'>You have completed the task ${titleText} at ${currentTime}</p>
+    `
+    activityList.appendChild(div);
+})
+document.getElementById('btn4').addEventListener('click', ()=>{
+    alert('Board updated Successfully.')
+    disableBtn('btn4');
+
+    valueIncrement();
+    valueDecrement();
+  
+    const currentDate = new Date();
+    const currentTime = currentDate.toLocaleTimeString();
+    const titleText = document.getElementById('title4').innerText;
+    const activityList = document.getElementById('activityList');
+    const div = document.createElement('div');
+    div.innerHTML=`
+    <p class='bg-gray-200 p-2 rounded-xl mt-2 text-start text-sm'>You have completed the task ${titleText} at ${currentTime}</p>
+    `
+    activityList.appendChild(div);
+})
+document.getElementById('btn5').addEventListener('click', ()=>{
+    alert('Board updated Successfully.')
+    disableBtn('btn5');
+
+    valueIncrement();
+    valueDecrement();
+  
+    const currentDate = new Date();
+    const currentTime = currentDate.toLocaleTimeString();
+    const titleText = document.getElementById('title6').innerText;
+    const activityList = document.getElementById('activityList');
+    const div = document.createElement('div');
+    div.innerHTML=`
+    <p class='bg-gray-200 p-2 rounded-xl mt-2 text-start text-sm'>You have completed the task ${titleText} at ${currentTime}</p>
+    `
+    activityList.appendChild(div);
 
 })
+document.getElementById('btn6').addEventListener('click', ()=>{
+    alert('Board updated Successfully.')
+    disableBtn('btn6');
+
+    valueIncrement();
+    valueDecrement();
+  
+    const currentDate = new Date();
+    const currentTime = currentDate.toLocaleTimeString();
+    const titleText = document.getElementById('title6').innerText;
+    const activityList = document.getElementById('activityList');
+    const div = document.createElement('div');
+    div.innerHTML=`
+    <p class='bg-gray-200 p-2 rounded-xl mt-2 text-start text-sm'>You have completed the task ${titleText} at ${currentTime}</p>
+    `
+    activityList.appendChild(div);
+})
+
 
 
 // remove activity log 
 document.getElementById('removeActivity').addEventListener('click', ()=>{
-    const removeChilds= document.getElementById('div');
+    const removeChilds= document.getElementById('activityList');
     removeChilds.innerHTML ='';
 })
